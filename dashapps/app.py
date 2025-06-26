8bd99061e597a990273e60e4bc507fec67ffb5c5
Harigovind T P <97115844+HariTP@users.noreply.github.com>
2025-06-26 19:49:42 +0530

    Update app.py

--- dashapps/app.py
+++ dashapps/app.py
@@ -1,14 +1,33 @@
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
+from dataflow.dataflow import Dataflow
 
 app = dash.Dash(__name__)
+
+dataflow = Dataflow()
+db = dataflow.connection("local_db")
+
+# Get values
+dummy = dataflow.variable("dummy")
+dummy_1 = dataflow.variable("dummy_local")
+dummy_2 = dataflow.variable("dummy_local_2")
+dummy_3 = dataflow.secret("dummy_secret")
+dummy_4 = dataflow.secret("dummy_secret_local")
 
 app.layout = html.Div([
     html.H1("Simple Dash App"),
     dcc.Input(id='input-name', type='text', placeholder='Enter your name'),
     html.Br(),
-    html.Div(id='output-greeting')
+    html.Div(id='output-greeting'),
+    html.Hr(),
+    html.H4("Debug Info"),
+    html.Pre(f"DB connection: {db}"),
+    html.Pre(f"dummy global variable: {dummy}"),
+    html.Pre(f"dummy local variable: {dummy_1}"),
+    html.Pre(f"dummy local variable 2: {dummy_2}"),
+    html.Pre(f"dummy global secret: {dummy_3}"),
+    html.Pre(f"dummy local secret: {dummy_4}")
 ])
 
 @app.callback(
