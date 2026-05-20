0f4302eadd743ca72a8cb654d9f0b13a47a0530c
Harigovind T P <97115844+HariTP@users.noreply.github.com>
2026-05-20 19:01:06 +0530

    Clean up print_api_key function
    Removed unused API key variables and connection logic.

--- airflow2/test.py
+++ airflow2/test.py
@@ -5,40 +5,21 @@ from airflow.hooks.base import BaseHook
 from datetime import datetime
 
 def print_api_key():
-    import tqdm
+    try:
+        import tqdm
+    except Exception as e:
+        print(e)
     try:
         import yfinance
     except Exception as e:
         print(e)
     # Fetch the variable
     api_key = Variable.get("news_api_key")
-    api_key_local = Variable.get("news_api_key_local")
-    api_key_secret = Variable.get("news_api_key_secret")
-    api_key_secret_local = Variable.get("news_api_key_secret_local")
-    if api_key_local=="hello_variable_local":
-        print("success ho gya1")
-    else:
-        print("fail1")
-    if api_key_secret=="hello":
-        print("success ho gya2")
-    else:
-        print("fail2")
     if api_key=="hello_variable":
         print("success ho gya3")
     else:
         print("fail3")
-    if api_key_secret_local=="hello_local":
-        print("success ho gya4")
-    else:
-        print("fail4")
     print(f"News API Key is: {api_key}")
-    print(f"News API Key secret is: {api_key_secret}")
-    conn_id = 'my_conn'
-    conn: Connection = BaseHook.get_connection(conn_id)
-    
-    # Assemble full DB URL
-    db_url = conn.get_uri()
-    print(f"Database connection URI = {db_url}")
 
 default_args = {
     'start_date': datetime(2023, 1, 1),
